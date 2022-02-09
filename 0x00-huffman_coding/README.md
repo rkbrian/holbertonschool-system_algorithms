e386b48a315898f8e3e078f1b01585eb482ec3fd
Ran Kong <rkbriankong@gmail.com>
2022-02-09 01:23:22 -0600

    setup

---
+++ 0x00-huffman_coding/README.md
@@ -0,0 +1,7 @@
+# 0x00. C - Huffman coding
+
+## General
+ - What is a binary Heap (min and max)
+ - What is a priority queue
+ - What is a Huffman code
+
---
+++ 0x00-huffman_coding/heap/heap_create.c
@@ -0,0 +1,11 @@
+#include "heap.h"
+
+/**
+ * heap_create - function that creates a Heap data structure
+ * @data_cmp: a pointer to a comparison function
+ * Return: a pointer to the created heap_t structure, or NULL if it fails
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	;
+}
