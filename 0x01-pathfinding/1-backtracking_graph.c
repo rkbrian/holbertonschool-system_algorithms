ae6dcd4264495a2dedd9a62ad82b23d914352bbc
Ran Kong <rkbriankong@gmail.com>
2022-02-21 13:06:23 -0600

    101

--- 0x01-pathfinding/1-backtracking_graph.c
+++ 0x01-pathfinding/1-backtracking_graph.c
@@ -32,12 +32,6 @@ queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 	ret_ptr = track_node(start, target, visited), free(visited);
 	if (!ret_ptr) /*track_nodesive function that return visited queue*/
 		return (NULL);
-	if (queue_push_front(ret_ptr, (void *)startcheck) == NULL)
-	{
-		queue_delete(ret_ptr), free(startcheck);
-		return (NULL); /*if target destination can't be reached*/
-	}
-	dequeue(ret_ptr);
 	return (ret_ptr);
 }
 
